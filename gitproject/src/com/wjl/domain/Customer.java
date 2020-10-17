c3ebe7ceef8d7dfd42135f1f6a5b891ee9ef39cd
wangjl <wangjianlong@126.com>
2020-10-18 00:09:25 +0800

    修改属性

--- gitproject/src/com/wjl/domain/Customer.java
+++ gitproject/src/com/wjl/domain/Customer.java
@@ -2,6 +2,7 @@ package com.wjl.domain;
 
 public class Customer {
 
+    private String name;
     private int num;
     public static void main(String[] args) {
         System.out.println("Hello");
